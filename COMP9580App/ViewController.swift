a24e13737a418bfc7f8f4dac5f42525c0ef1db28
Alx Barker <alx.barker@gmail.com>
2015-03-12 16:11:54 -0400

    Initial Commit

---
+++ COMP9580App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  COMP9580App
+//
+//  Created by Alx Barker on 2015-03-12.
+//  Copyright (c) 2015 Alx Barker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
